2012d0ec035d3797743f8d1a399a2b118244e31a
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2026-03-05 17:50:04 +0000

    tic tac toe beginning to work

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -1,9 +1,36 @@
-def tic_tac():
-    print("Welcome to Tic-Tac-Toe!")
+#EHCP2 tic tac
 
-    board = [' ' for x in range(9)]
+board = [x for x in range(9)]
 
-    win_conds = [(0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6)]
-    def choices():
-        letter = input("What is the letter that you want to be?\nIt can be X or O, but it could also be anything else\nType Here:")
+letter = input("What is the letter that you want to be?\nIt can be X or O, but it could also be anything else\nType Here:")
+
+def print_board():
+    for i in range(0,9,3):
+        print({board[i]}, "|", {board[i+1]}, "|", {board[i+2]})
+        if i < 6:
+            print("---------------")
+
         
+def win(letter):
+    win_conds = [(0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6)]
+    return any(board[a] == board[b] == board[c] == letter for a, b, c in win_conds)
+
+
+
+def play():
+    print("Welcome to Tic-Tac-Toe!")
+    print_board()
+    while True:
+        try:
+            placement = int(input("Now, where would you like to play?"))
+
+            if placement in board:
+                index = board.index(placement)
+                board[index] = placement
+
+            else:
+                print("That ain't somewhere you can place!")
+        except ValueError:
+            print("That ain't somewhere you can place!")
+
+play()
